14d1aa4babdf853ca6c8f7ec05cfb3bd6f79f574
scruffy-node-9597 <gordonbrian16@gmail.com>
2020-06-06 18:26:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   new_array = []
   array.length.times do |index|
-  new_array << (array[index] ** 2)
+  new_array << array[index] ** 2
   puts new_array
  end
-
+new_array
 end
